4be7e33a518215083246ce3aa6dfd177ff56c7b5
02MARIII <MARISHHHALLL@gmail.com>
2023-09-08 15:16:04 +0100

    add for loop for print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: printing all alphabet character
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char first = 'a'
+
+	for (first = 'a'; first <= "z"; first++)
+	{
+		putchar(first)
+	}
+	putchar("\n")
+	return (0);
+{
