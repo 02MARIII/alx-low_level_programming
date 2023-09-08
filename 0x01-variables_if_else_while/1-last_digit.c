75669f12d72d6e30c848bb54b28c9b3225ed555f
02MARIII <MARISHHHALLL@gmail.com>
2023-09-08 14:36:07 +0100

    add if statement on last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-	if (last_digit > 5)
+	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-	else if (last_digit < 6 && last_digit != 0)
+	else if (digit < 6 && digit != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	else
 		printf("Last digit of %d is %d and is 0", n, digit);
