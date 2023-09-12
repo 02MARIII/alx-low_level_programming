25a7f2bbc9fd402772a3444bc281d547650f7f52
02MARIII <MARISHHHALLL@gmail.com>
2023-09-12 14:04:10 +0100

    add alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,20 +10,10 @@ void print_alphabet(void)
 {
 	char a = 'a';
 
-	while (a < 'z')
+	while (a <= 'z')
 	{
 		_putchar(a);
 		a++;
 	}
 	_putchar('\n');
-}
-/**
- * main - Entry point
- * description: the description
- * Return: Always 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
