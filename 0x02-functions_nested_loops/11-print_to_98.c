c272740c3dd05a34bdd6313fff03d157854f7790
02MARIII <MARISHHHALLL@gmail.com>
2023-09-12 18:20:26 +0100

    fix print to 98 task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,18 +17,22 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
-	printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ",i);
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
-	printf("\n");
 	}
 	else if (n == 98)
-		printf("%d\n",n);
+		printf("%d\n", n);
 }
