548d4cf7eacf0f9938e5e2721bbb9856274dc8da
02MARIII <MARISHHHALLL@gmail.com>
2023-09-14 09:18:07 -0700

    add more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "main.h"
-
 /**
- * main - check the code
- * description: the description
- * Return: Always 0.
+ *more_numbers - writes the character c to stdout
  */
 void more_numbers(void)
 {
-	int i, j;
+	int a, t;
 
-	for (i = 0; i < 9; i++)
+	for (t = 0; t <= 9; t++)
 	{
-		j = 0;
-		for (j = 0; j <= 14; j++)
+		for (a = 0; a <= 14; a++)
 		{
-			if (j > 9)
+			if (a > 9)
 			{
-				_putchar(j / 10 + 48);
+				_putchar(a / 10 + 48);
 			}
-			_putchar(j % 10 + 48);
+			_putchar(a % 10 + 48);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
