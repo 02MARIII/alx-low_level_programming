15b74739d31dbf6727f7cd7c4595c3b8c01a2c4d
02MARIII <MARISHHHALLL@gmail.com>
2023-09-18 18:14:36 +0100

    add print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @s: char param
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+	int n = 0;
+
+	while (s[x])
+		x++;
+	for (n = x; n >= 0; n--)
+		putchar(s[n]);
+	putchar('\n');
+		
+}
