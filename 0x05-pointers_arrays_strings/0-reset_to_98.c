84472d9dd19d920ec669f53f28fdde85ace455f8
02MARIII <MARISHHHALLL@gmail.com>
2023-09-18 15:57:07 +0100

    add first task 0-reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * @n: int param
+ * description: the descirption
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	int *p = *n;
+
+	*p = 98;
+
+}
