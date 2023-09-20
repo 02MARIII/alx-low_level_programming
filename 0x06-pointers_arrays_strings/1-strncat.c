6f8f4619b90051cc3754f2a2d4c22b7aababd9e9
02MARIII <MARISHHHALLL@gmail.com>
2023-09-20 15:58:50 +0100

    add strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - Entry point
+ * @dest: char param
+ * @src: char param
+ * @n: int param
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (src[j] && j < n)
+	{
+		dest[i++] = src[j];
+		j++;
+	}
+	return (dest);
+}
