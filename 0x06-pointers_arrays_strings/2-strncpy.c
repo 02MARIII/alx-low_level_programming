a687778ce91cf9d57c8d9e90fffe82f5107b9110
02MARIII <MARISHHHALLL@gmail.com>
2023-09-20 16:53:01 +0100

    add strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strncpy - Entry point
+ * @dest: char param
+ * @src: char param
+ * @n: int param
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] < n && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if(i < n)
+		dest[i + 1] = '\n';
+	return (dest);
+}
