b347e25961531bf21855d6cf790d25c2da736a3e
02MARIII <MARISHHHALLL@gmail.com>
2023-09-25 16:51:55 +0100

    add strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: pointer of c
+ * @c: param of char
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+	if (s[i] == c)
+	{
+		return (&(s[i]));
+	}
+	i++;
+	}
+	return (0);
+}
