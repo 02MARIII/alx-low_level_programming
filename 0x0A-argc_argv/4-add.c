9f74ee62c44ee78f13b31fbb93691c1af78344c5
02MARIII <MARISHHHALLL@gmail.com>
2023-10-02 23:58:35 +0100

    add last task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,44 +1,50 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * main - Entry point
- * @argc: index of argv
- * @argv: list of strings in argc
+ * is_positive_number - Entry point
+ * @str: pointer
  * Return: 0 Always
  */
+int is_positive_number(char *str)
+{
+	int i;
+	int num;
 
-int main(int argc, char *argv[])
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+	}
+	num = atoi(str);
+
+	return (num > 0);
+}
+/**
+ * add_positive_numbers - Entry point
+ * @argc: index of list argv
+ * @argv: list of strings
+ * Return: Always 0
+ */
+int add_positive_numbers(int argc, char *argv[])
 {
 	int total = 0;
 	int error_flag = 0;
 	int i;
-	int j;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					error_flag = 1;
-					break;
-				}
-			}
-			if (!error_flag)
-			{
-				int num = atoi(argv[i]);
+	int num;
 
-				if (num <= 0)
-				{
-					printf("Error\n");
-					error_flag = 1;
-					break;
-				}
-				total += num;
-			}
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+			printf("Error\n");
+			error_flag = 1;
+			break;
 		}
+		num = atoi(argv[i]);
+
+		total += num;
 	}
 
 	if (error_flag)
@@ -48,6 +54,24 @@ int main(int argc, char *argv[])
 	else
 	{
 		printf("%d\n", total);
+		return (0);
+	}
+}
+/**
+ * main - Entry point
+ * @argc: index of list argv
+ * @argv: list of strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		return (add_positive_numbers(argc, argv));
+	}
+	else
+	{
+		printf("0\n");
 		return (0);
 	}
 }
