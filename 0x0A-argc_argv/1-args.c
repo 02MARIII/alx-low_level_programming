09224da225e594a4a3e3f74cfd0d4a56f3a63958
02MARIII <MARISHHHALLL@gmail.com>
2023-10-02 21:39:49 +0100

    add sec task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: index of argv
+ * @argv: list of strings in argc
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
