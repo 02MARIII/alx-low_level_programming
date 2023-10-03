98e8b043b0ac88aceeeb977f44ab171bd87d5cf3
02MARIII <MARISHHHALLL@gmail.com>
2023-10-03 18:01:05 +0100

    add 2-concat

---
+++ 0x0B-malloc_free/test.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * *str_concat - Entry point
+ * @s1: param of char
+ * @s2: param of char
+ * Return: char
+*/
+char *str_concat(char *s1, char *s2)
+{
+    int x = 0, i = 0, j, k;
+    char *buff;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+    while (s1[x] != '\0')
+        x++;
+    while (s2[i] != '\0')
+        i++;
+    buff = malloc(sizeof(char) * (x + i + 1));
+    if (buff == NULL)
+        return (NULL);
+    for (j = 0; j < x; j++)
+    {
+        buff[j] = s1[j];
+    }
+    for (k = 0; k < i; k++)
+    {
+        buff[x + k] = s2[k];
+    }
+    buff[x + i] = '\0';
+    return (buff);
+}
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
+}
