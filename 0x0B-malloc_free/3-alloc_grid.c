eddbfd914fe08cf75a41e2c90a842e3fae976826
02MARIII <MARISHHHALLL@gmail.com>
2023-10-03 18:42:58 +0100

    fix spaces on task alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,19 +9,22 @@ int **alloc_grid(int width, int height)
 {
 	int i, j, k;
 	int **grid;
-	if (width <= 0 || height <= 0) {
+
+	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
 	}
 	grid = malloc(height * sizeof(int *));
-	
-	if (grid == NULL) {
+	if (grid == NULL)
+	{
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
-		if (grid[i] == NULL) {
+		if (grid[i] == NULL)
+		{
 			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
