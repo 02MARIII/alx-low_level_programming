47d986d1d6d3db290a71ab44d0c5cf87ac31d340
02MARIII <MARISHHHALLL@gmail.com>
2023-10-06 22:31:16 +0100

    fix third task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,19 +11,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *dep;
 	int total;
 
-	if (nmemb == 0 || size == 0) {
-        return (NULL);
-    }
-    total = nmemb * size;
-    dep = malloc(total);
-
-    if (dep == NULL) {
-        return (NULL);
-    }
-    for (i = 0; i < total; i++)
+	if (nmemb == 0 || size == 0)
 	{
-        *((char *)dep + i) = 0;
-    }
+		return (NULL);
+	}
+	total = nmemb * size;
+	dep = malloc(total);
 
-    return (dep);
+	if (dep == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < total; i++)
+	{
+		*((char *)dep + i) = 0;
+	}
+
+	return (dep);
 }
