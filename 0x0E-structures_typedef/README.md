7fb5bf4085eea82b2b2b3471d605751752d97dbe
02MARIII <MARISHHHALLL@gmail.com>
2023-10-10 17:00:08 +0100

    add dog header

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this readme for struct and typdef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for dog
+ * @name: pointer param for char
+ * @age: param for float
+ * @owner: pointer param for char
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
