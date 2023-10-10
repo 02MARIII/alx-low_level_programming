bc7b4e9fe4336ef78755414b9b51abafb885de2e
02MARIII <MARISHHHALLL@gmail.com>
2023-10-10 13:53:33 +0100

    add task 2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: the description
+ * Return: int
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIH_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+#endif
