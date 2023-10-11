e6c83402de9e894d1394a1388a580a209d106e41
02MARIII <MARISHHHALLL@gmail.com>
2023-10-11 16:00:57 +0100

    add new dir + first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ * @name: param for char
+ * @f: function pointer
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	/* void (*pointer_f)(char *); */
+
+	if (f)
+		return;
+	(*f)(name);
+	/* pointer_f = f; */
+	/* (*pointer_f)(name);*/
+}
