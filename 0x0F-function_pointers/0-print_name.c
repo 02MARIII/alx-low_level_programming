71a70728b9ee0eac05e2b266453cb86ae2f157ce
02MARIII <MARISHHHALLL@gmail.com>
2023-10-11 16:12:01 +0100

    fix first task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,9 +9,8 @@ void print_name(char *name, void (*f)(char *))
 {
 	/* void (*pointer_f)(char *); */
 
-	if (f == NULL || name == NULL)
-		return;
-	(*f)(name);
+	if (f || name)
+		(*f)(name);
 	/* pointer_f = f; */
 	/* (*pointer_f)(name);*/
 }
