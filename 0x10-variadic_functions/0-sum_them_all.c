04175e6f6c8964ceead9a33ecc1eb8f482c95408
02MARIII <MARISHHHALLL@gmail.com>
2023-10-12 15:15:30 +0100

    add task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Entry point
+ * @n: param for unsigned int
+ * Return: int
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int s = 0;
+	unsigned int i, x;
+
+	va_list args;
+	va_start(args, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, unsigned int);
+		s += x;
+	}
+	va_end(args);
+	return (s);
+}
