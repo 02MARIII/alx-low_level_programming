6e64c0b7ddc177e6ce025890e24298af08c2aa6b
02MARIII <MARISHHHALLL@gmail.com>
2023-10-19 16:19:53 +0100

    :skull: forget betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,13 @@
 #include "lists.h"
-
+/**
+ * print_list - Entry point
+ * @h: const param for list_t
+ * Return: size_t
+*/
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
+
 	while (h)
 	{
 		if (!h->str)
