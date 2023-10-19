7b56d1d79c4c5258a00268e6c4a7ea214d690835
02MARIII <MARISHHHALLL@gmail.com>
2023-10-19 16:18:14 +0100

    remove strlen cuz already having h->len

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,12 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-	int z = 0;
-
-	for (z = 0; h->str[z] != '\0'; z++)
-		;
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", z, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
