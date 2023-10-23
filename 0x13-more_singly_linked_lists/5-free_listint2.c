833bb8847977c225d188ed6a67ebed276132d2cf
02MARIII <MARISHHHALLL@gmail.com>
2023-10-23 16:52:53 +0100

    fix task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 /**
- * free_listint - Entry point
- * @head: param for listint_t
+ * free_listint2 - Entry point
+ * @head: double pointer param for listint_t
  * Return: void
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp = *head;
+	listint_t *ser;
 
-	if (head != NULL)
+	while (ser != NULL)
 	{
-		temp = *head;
-		while (temp != NULL)
-		{
-			temp = temp->next;
-			free(temp);
-		}
-		*head = NULL;
+		temp = ser;
+		ser = ser->next;
+		free(temp);
 	}
+	*head = NULL;
 }
