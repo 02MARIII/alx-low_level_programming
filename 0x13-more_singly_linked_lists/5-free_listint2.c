74ca1c41f76fbaa3e5d259c3c892953448412b23
02MARIII <MARISHHHALLL@gmail.com>
2023-10-23 17:36:51 +0100

    last try

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head)
+	while (*head)
 	{
-		while (*head)
-		{
-			temp = *head;
-			*head = (*head)->next;
-			free(temp);
-		}
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 	free(*head);
-	head = NULL;
+	head = 0;
 }
