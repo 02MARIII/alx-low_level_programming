0958ccb5d621d47dd8dc88ff70c5163f3179a5d4
02MARIII <MARISHHHALLL@gmail.com>
2023-10-23 17:41:55 +0100

    test

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
 	free(*head);
-	head = 0;
+	*head = NULL;
 }
