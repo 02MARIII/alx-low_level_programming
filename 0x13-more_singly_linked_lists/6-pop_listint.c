944c524aca86292ee095c4ec51462cfe26e0dcbf
02MARIII <MARISHHHALLL@gmail.com>
2023-10-23 19:34:14 +0100

    try this one

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,13 +8,13 @@ int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head)
+	if (head == NULL)
 	{
 		return (0);
 	}
 	else
 	{
-		temp = (*head);
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 		temp = NULL;
