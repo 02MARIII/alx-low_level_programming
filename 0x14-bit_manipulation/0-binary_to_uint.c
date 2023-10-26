2eed9aa0317db029f7057aeb8cc3adfaaf8a2671
02MARIII <MARISHHHALLL@gmail.com>
2023-10-26 16:34:01 +0100

    try this one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,14 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-	val <<= 1;
-	val += b[i] - '0';
-	i++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
 	}
+
 	return (val);
 }
