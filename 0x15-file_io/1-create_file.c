629389a30880a1be24208fc378fc4b4252c84e5c
02MARIII <MARISHHHALLL@gmail.com>
2023-10-30 23:20:01 +0100

    one more test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		bytes_written = write(fileDes, text_content, strlen(text_content));
-		if (bytes_written == -1) {
+		if (bytes_written == -1)
+		{
 			close(fileDes);
 			return (-1);
 		}
