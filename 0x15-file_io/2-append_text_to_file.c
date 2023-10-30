3c13d1a7b1d198c8004fab07afe8cac9d0bc0976
02MARIII <MARISHHHALLL@gmail.com>
2023-10-31 00:05:46 +0100

    add new task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Entry point
+ * @filename: const pointer param for char
+ * @text_content: pointer param for char
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int openFile, writeFile;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+	}
+	openFile = open(filename, O_WRONLY | O_APPEND);
+	writeFile = write(openFile, text_content, len);
+	if (openFile == -1 || writeFile == -1)
+		return (-1);
+	close(openFile);
+	return (1);
+}
