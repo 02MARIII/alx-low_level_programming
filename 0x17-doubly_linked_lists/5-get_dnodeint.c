af6d924713727385c2cf8265f5fb5bf78cede0d4
02MARIII <MARISHHHALLL@gmail.com>
2023-12-08 00:45:15 +0100

    add new task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Entry point
+ * @head: pointer param for dlistint_t
+ * @index: unsigned param for int
+ * Return: dlistint_t
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *current = head;
+    unsigned int i;
+
+    if (current != NULL)
+    {
+        while (current->prev != NULL)
+            current = current->prev;
+
+        for (i = 0; i < index && current != NULL; i++)
+            current = current->next;
+
+        return (current);
+    }
+    else
+    {
+        return (NULL);
+    }
+}
