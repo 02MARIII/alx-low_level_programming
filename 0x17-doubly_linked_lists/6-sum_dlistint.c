0d1850d03f36c4c4b84eb24de93704fdad131e77
02MARIII <MARISHHHALLL@gmail.com>
2023-12-08 01:02:38 +0100

    add new task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Entry point
+ * @head: pointer param for dlistint_t
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *current = head;
+    int sum = 0;
+
+    if (current != NULL)
+    {
+        while (current->prev != NULL)
+            current = current->prev;
+
+        while (current != NULL)
+        {
+            sum += current->n;
+            current = current->next;
+        }
+        return sum;
+    }
+    else
+    {
+        return 0;
+    }
+}
