9270ad6018acc4512628306fed8d2735d6d53028
02MARIII <MARISHHHALLL@gmail.com>
2023-12-08 01:04:18 +0100

    add new task

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,23 +6,23 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = head;
-    int sum = 0;
+	dlistint_t *current = head;
+	int sum = 0;
 
-    if (current != NULL)
-    {
-        while (current->prev != NULL)
-            current = current->prev;
+	if (current != NULL)
+	{
+		while (current->prev != NULL)
+			current = current->prev;
 
-        while (current != NULL)
-        {
-            sum += current->n;
-            current = current->next;
-        }
-        return sum;
-    }
-    else
-    {
-        return 0;
-    }
+		while (current != NULL)
+		{
+			sum += current->n;
+			current = current->next;
+		}
+		return sum;
+	}
+	else
+	{
+		return 0;
+	}
 }
