4d56be4c49e12136cd4794cc301914900069d2c2
02MARIII <MARISHHHALLL@gmail.com>
2023-12-22 20:35:42 +0100

    try with the while

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
 
-	for (; current_node != NULL; current_node = current_node->next)
+	while (current_node)
 	{
 		if (strcmp(current_node->key, key) == 0)
 			return (current_node->value);
