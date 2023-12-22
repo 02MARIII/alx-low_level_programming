d956027b6f87bb5673bc7d5d9ef589994a81bcfc
02MARIII <MARISHHHALLL@gmail.com>
2023-12-22 20:34:06 +0100

    fix something

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,17 +9,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *current_node = NULL;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
+
 	for (; current_node != NULL; current_node = current_node->next)
 	{
 		if (strcmp(current_node->key, key) == 0)
 			return (current_node->value);
+		current_node = current_node->next;
 	}
 
 	return (NULL);
